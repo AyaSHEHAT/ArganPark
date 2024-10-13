6497770bea25247a5085e5f8b58e8d35c43cd7b4
omarabdallah10 <omarabdallah716@yahoo.com>
2024-10-13 14:00:09 +0300

    nav refactoring - Omar phase

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+//starting as index.js 
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    let changeLangToEnglish = document.getElementById('change-lang-to-en');
+    //make it on click to change the html page to another page from the same folder
+    changeLangToEnglish.addEventListener('click', function () {
+        window.location.href = 'confirmation.html';
+    });
+    let changeLangToArabic = document.getElementById('change-lang-to-ar');
+
+
+});
