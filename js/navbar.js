22785c86cb0ab644a39f21a0e978084d4e3f952b
Aya Shehata <ayashehata274@yahoo.com>
2024-10-16 00:08:32 +0300

    login and signup

--- js/navbar.js
+++ js/navbar.js
@@ -1,33 +1,69 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let changeLangToEnglish = document.getElementById('change-lang-to-en')?null:document.getElementById('change-lang-to-ar');
+  let changeLangToEnglishDeclartion = document.getElementById('change-lang-to-en');
+  let changeLangToEnglish = changeLangToEnglishDeclartion?changeLangToEnglishDeclartion:document.getElementById('change-lang-to-ar');
   // Change language to English on click
   changeLangToEnglish.addEventListener('click', function () {
       window.location.href = 'confirmation.html';
   });
 
-  const emailBtn = document.querySelector('.loginWithMail');
-  const phoneBtn = document.querySelector('.loginWithPhone');
-  const phoneDiv = document.querySelector('.phoneDiv');
+  const loginEmailBtn = document.querySelector('.loginWithMail');
+  const loginPhoneBtn = document.querySelector('.loginWithPhone');
+//   const phoneDiv = document.querySelector('.phoneDiv');
   const emailDiv = document.querySelector('.emailDiv');
+  const loginDivWithPhone = document.querySelector('.loginDivWithPhone');
+  const loginDivWithEmail = document.querySelector('.loginDivWithEmail');
+  
+  const signemailBtn = document.querySelector('.signWithMail');
+  const signphoneBtn = document.querySelector('.signWithPhone');
+  const btnLogin = document.querySelector('button.login');
+  const logInForm = document.querySelector('form.logInForm'); //by default is 'd-none'
+  const btnSignUp = document.querySelector('button.signup');
+  const signUpForm = document.querySelector('form.signUpForm');
 
-  // Toggle between email and phone form fields
-  emailBtn.addEventListener('click', function () {
-      if (!emailBtn.classList.contains('active')) {
-          emailBtn.classList.add('active');
-          phoneBtn.classList.remove('active');
-          phoneDiv.classList.add('d-none');
+btnLogin.addEventListener('click', function () {
+    signUpForm.classList.add('d-none');
+       logInForm.classList.remove('d-none');
+});
+  // Toggle between email and phone form fields in sign up form
+  signemailBtn.addEventListener('click', function () {
+      if (!signemailBtn.classList.contains('active')) {
+        signemailBtn.classList.add('active');
+        signphoneBtn.classList.remove('active');
+         // phoneDiv.classList.add('d-none');
           emailDiv.classList.remove('d-none');
       }
   });
-
-  phoneBtn.addEventListener('click', function () {
-      if (!phoneBtn.classList.contains('active')) {
-          phoneBtn.classList.add('active');
-          emailBtn.classList.remove('active');
-          phoneDiv.classList.remove('d-none');
+  signphoneBtn.addEventListener('click', function () {
+      if (!signphoneBtn.classList.contains('active')) {
+        signphoneBtn.classList.add('active');
+        signemailBtn.classList.remove('active');
+         // phoneDiv.classList.remove('d-none');
           emailDiv.classList.add('d-none');
       }
   });
+
+  // Toggle between email and phone form fields in login form
+  loginEmailBtn.addEventListener('click', function () {
+      if (!loginEmailBtn.classList.contains('active')) {
+        loginEmailBtn.classList.add('active');
+        loginPhoneBtn.classList.remove('active');
+        loginDivWithEmail.classList.remove('d-none');
+        loginDivWithPhone.classList.add('d-none');        
+      }
+  });
+  loginPhoneBtn.addEventListener('click', function () {
+      if (!loginPhoneBtn.classList.contains('active')) {
+        loginPhoneBtn.classList.add('active');
+        loginEmailBtn.classList.remove('active');
+        loginDivWithEmail.classList.add('d-none');
+        loginDivWithPhone.classList.remove('d-none');
+      }
+  });
+  btnSignUp.addEventListener('click', function () {
+    signUpForm.classList.remove('d-none');
+    logInForm.classList.add('d-none');
+  });
+
 
   const userLogo = document.getElementById('userLogo');
   const userMenu = document.getElementById('userMenu');
